788dbbd23503c3aa4898298a9727fd9ab6676d2f
Tomas2054 <112587445+Tomas2054@users.noreply.github.com>
2023-09-08 19:22:21 -0300

    Homework 01

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -13,94 +13,130 @@ function devolverString(Tomas) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+    var suma = x + y
+    return suma
   }  
   
   function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
-  }
+  var resta = x - y
+return resta
+}
   
   function multiplica(x, y) {
     // Multiplica "x" por "y" y devuelve el valor
     // Tu código:
-  }
+  var multiplica = x*y
+return multiplica
+}
   
   function divide(x, y) {
     // Divide "x" entre "y" y devuelve el valor
     // Tu código:
-  }
+ var divide = x/y
+return divide
+}
   
   function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
-  }
+if (x===y) {
+  return true
+}
+else {
+  return false
+ }}
   
   function tienenMismaLongitud(str1, str2) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+  if (str1.lenght === str2.lenght){
+    return true
   }
+  else {
+    return false
+  }}
   
   function menosQueNoventa(num) {
     // Devuelve "true" si el argumento de la función "num" es menor que noventa
     // De lo contrario, devuelve "false"
     // Tu código:
-  }
+  if (num < 90) {
+    return true
+  } 
+  else {
+    return false
+  }}
   
   function mayorQueCincuenta(num) {
     // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
     // De lo contrario, devuelve "false"
     // Tu código:
-  }
+    if (num > 50){
+      return true}
+    else {
+      return false
+    }}
   
   function obtenerResto(x, y) {
     // Obten el resto de la división de "x" entre "y"
     // Tu código:
-  }
+  var resto = x % y
+return resto}
   
   function esPar(num) {
     // Devuelve "true" si "num" es par
     // De lo contrario, devuelve "false"
     // Tu código:
-  }
+    
+    return (num % 2) === 0;
+    }
   
   function esImpar(num) {
     // Devuelve "true" si "num" es impar
     // De lo contrario, devuelve "false"
     // Tu código:
+    return (num % 2) != 0
   }
   
   function elevarAlCuadrado(num) {
     // Devuelve el valor de "num" elevado al cuadrado
     // ojo: No es raiz cuadrada!
     // Tu código:
+    return Math.pow (num,2)
   }
   
   function elevarAlCubo(num) {
     // Devuelve el valor de "num" elevado al cubo
     // Tu código:
+    return Math.pow (num,3)
   }
   
   function elevar(num, exponent) {
     // Devuelve el valor de "num" elevado al exponente dado en "exponent"
     // Tu código:
+    return Math.pow (num, exponent)
   }
   
   function redondearNumero(num) {
     // Redondea "num" al entero más próximo y devuélvelo
     // Tu código:
-  }
+ return Math.round(num) }
   
   function redondearHaciaArriba(num) {
     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
     // Tu código:
+    return Math.ceil(num)
   }
   
   function numeroRandom() {
     //Generar un número al azar entre 0 y 1 y devolverlo
     //Pista: investigá qué hace el método Math.random()
     // Tu código:
+    return Math.random()
   }
   
   function esPositivo(numero) {
@@ -109,12 +145,21 @@ function devolverString(Tomas) {
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
+  if (numero > 0){
+    return 'Es positivo'
   }
+  else if (numero === 0) {
+    return false
+  } else {
+    return 'Es negativo'
+   }}
   
-  function agregarSimboloExclamacion(str) {
+  
+   function agregarSimboloExclamacion(str) {
     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
     // Ejemplo: "hello world" pasaría a ser "hello world!"
-    // Tu código:
+     // Tu código:
+    return str + '!'
   }
   
   function combinarNombres(nombre, apellido) {
---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+// Write a message to the console.
+console.log('hello world!');
+
+var a = 1, b = 2, c = 3;
+
+a = b = c;
+
+console.log(a, b, c);
+
+function logHola(nombre) {
+    console.log('Hola, ' + nombre);
+}
+
+logHola('Martin');
+
+function sumarDosNumeros(a, b) {
+    var suma = a + b;
+    return suma;
+  }
+  
+  sumarDosNumeros(2, 6); 
+  
