ea2da6a195b67538ce603cd55ee2f731c848cb4f
Tomas2054 <112587445+Tomas2054@users.noreply.github.com>
2023-09-11 11:59:10 -0300

    Homeworks 02

--- ejemplo.js
+++ ejemplo.js
@@ -1,1 +1,9 @@
-(100 > 10) || (10 === 10)
+(100 > 10) || (10 === 10);
+
+var PrimerPalabra = 'Hola';
+var SegundaPalabra = ' Mundo';
+var resultado = PrimerPalabra + SegundaPalabra;
+
+console.log(resultado);
+
+function Hola () 
